47e6f92f0a2740250ece268cde982d19be8dcb1c
Ian Fleming <iantfleming1992@gmail.com>
2020-10-24 14:01:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,7 @@ class TicTacToe
     index = input_to_index(input)
     token = input
     if valid_move?(index)
-      move(index, current_player(@board))
+      move(index, token="X")
       display_board
     else
       turn
