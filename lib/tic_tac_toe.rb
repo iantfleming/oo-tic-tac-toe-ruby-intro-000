0e1edd4b4db068c96764a27d4af1d85f9b27b0b4
Ian Fleming <iantfleming1992@gmail.com>
2020-10-24 14:05:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,7 +54,15 @@ class TicTacToe
   end
   
   
-  
+  def turn_count(board)
+    counter = 0
+    board.each do |space| 
+    if space == "X" || space == "O"
+      counter += 1
+      end
+    end
+    counter  
+  end
   
   
   
