88c67b09a79242d10153b12da4821e4ef9941539
Ian Fleming <iantfleming1992@gmail.com>
2020-10-24 14:28:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -132,11 +132,11 @@ class TicTacToe
   
   def winner
     index = []
-    index = won?(board)
+    index = won?
     if index == false
       return nil
     else
-      if board[index[0]] == "X"
+      if @board[index[0]] == "X"
         return "X"
       else
         return "O"
