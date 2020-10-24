1f2733596b49b88c7d340954f8648daf590fc564
Ian Fleming <iantfleming1992@gmail.com>
2020-10-24 14:11:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,7 +89,7 @@ class TicTacToe
   end
   
   
-  def won?(board)
+  def won?
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
