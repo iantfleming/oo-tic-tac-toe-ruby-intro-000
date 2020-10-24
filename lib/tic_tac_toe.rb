216f59950bb103ab7f060eef7272a8327b28ff79
Ian Fleming <iantfleming1992@gmail.com>
2020-10-24 13:49:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,8 +47,8 @@ class TicTacToe
   end
   
   
-  def valid_move?(board, index)
-    if index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(index)
+    if index.between?(0,8) && !position_taken?(@board, index)
       return true
     end
   end
